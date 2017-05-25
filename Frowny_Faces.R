c3df2d5f3bc3fe2cd34ab9f37c8813c571f4ce14
rjake <rjake@sas.upenn.edu>
2017-05-25 14:09:11 -0400

    Update and rename NPS_Faces.R to Frowny_Faces.R

--- Frowny_Faces.R
+++ Frowny_Faces.R
@@ -21,34 +21,34 @@ plot(a$x, a$y0)
 
 b <-
   data.frame(team = letters[1:11],
-             NPS = seq(-100, 100, length.out = 11),
+             Score = seq(-100, 100, length.out = 11),
              join = 1) %>%
   right_join(a) %>%
   mutate(absy = y0*100,
-         absNPS = abs(NPS)) %>% 
-  filter(absy <= absNPS) %>%
-  mutate(dir = ifelse(NPS < 0, -1, 1),
-         width = absNPS/101,
+         absScore = abs(Score)) %>% 
+  filter(absy <= absScore) %>%
+  mutate(dir = ifelse(Score < 0, -1, 1),
+         width = absScore/101,
          y2 = y*dir,
          x2 = ifelse(x < 0, 
                      x - abs(x)*width, 
                      x + abs(x)*width)
-         )
+  )
 
-  ggplot(b)+
-    facet_wrap(~ team, nrow = 3) +
-    theme(axis.title = element_blank(),
-          axis.text = element_blank(),
-          axis.ticks = element_blank(),
-          panel.background = element_rect(fill = "white")) +
-    geom_point(aes(x = .5, y = .25, fill = NPS), shape = 21, size = 35) +
-    scale_fill_continuous(low = "darkorange", high = "yellow") +
-    geom_point(aes(x = -.5, y = 1.5), shape = 21, size = 3) +
-    geom_point(aes(x = .5, y = 1.5), shape = 21, size = 3) +
-    geom_line(aes(x = x2, y = y2)) +
-    ylim(-2.5, 2.5) +
-    xlim(-2.5, 2.5) +
-    coord_fixed()
+ggplot(b)+
+  facet_wrap(~ team, nrow = 3) +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.background = element_rect(fill = "white")) +
+  geom_point(aes(x = .5, y = .25, fill = Score), shape = 21, size = 40) +
+  scale_fill_continuous(low = "darkorange", high = "yellow") +
+  geom_point(aes(x = -.5, y = 1.5), shape = 21, size = 3) +
+  geom_point(aes(x = .5, y = 1.5), shape = 21, size = 3) +
+  geom_line(aes(x = x2, y = y2)) +
+  ylim(-2.5, 2.5) +
+  xlim(-2.5, 2.5) +
+  coord_fixed()
 
 ##################### historical ##############################  
 
@@ -58,27 +58,27 @@ df <-
               team = LETTERS[1:4],
               KEEP.OUT.ATTRS = F) %>% 
   rowwise() %>% 
-  mutate(NPS = ifelse(month == 1,
+  mutate(Score = ifelse(month == 1,
                       sample(c(-50:80), 1),
                       sample(c(-15:15), 1)),
          join = 1) %>% 
   ungroup() %>% 
   group_by(team) %>% 
-  mutate(NPS = cumsum(NPS)) %>% 
+  mutate(Score = cumsum(Score)) %>% 
   ungroup() %>%
   right_join(a) %>%
   mutate(absy = y0*100,
-         absNPS = abs(NPS)) %>% 
-  filter(absy <= absNPS) %>%
-  mutate(dir = ifelse(NPS < 0, -1, 1),
-         width = absNPS/101,
+         absScore = abs(Score)) %>% 
+  filter(absy <= absScore) %>%
+  mutate(dir = ifelse(Score < 0, -1, 1),
+         width = absScore/101,
          y2 = y*dir,
          x2 = ifelse(x < 0, 
                      x - abs(x)*width, 
                      x + abs(x)*width)
   )
 
-ggplot(df, aes(month, NPS, color = team, group = team))+
+ggplot(df, aes(month, Score, color = team, group = team))+
   geom_line(size = 2)
 
 ggplot(df)+
@@ -87,29 +87,29 @@ ggplot(df)+
         axis.text = element_blank(),
         axis.ticks = element_blank(),
         panel.background = element_rect(fill = "grey90")) +
-  geom_point(aes(x = .5, y = .25, fill = NPS), 
-             shape = 21, size = 25, color = "black", stroke = 1) +
+  geom_point(aes(x = .5, y = .25, fill = Score), 
+             shape = 21, size = 20, color = "black", stroke = 1) +
   scale_fill_continuous(low = "darkorange", high = "yellow") +
   geom_point(aes(x = -.5, y = 1.5), shape = 21, size = 3) +
   geom_point(aes(x = .5, y = 1.5), shape = 21, size = 3) +
-  geom_line(aes(x = x2, y = y2), size = 1) +
-  ylim(-2.5, 2.5) +
-  xlim(-2.5, 2.5) +
+  geom_line(aes(x = x2, y = y2)) +
+  ylim(-3, 3) +
+  xlim(-3, 3) +
   coord_fixed()
 
 df %>% 
   filter(team == "C") %>% 
   ggplot()+
   geom_hline(aes(yintercept = 0), size = 1, alpha = .3, linetype = "dashed") +
-  geom_line(aes(month, NPS), size = 5, alpha = .3) +
-  geom_point(aes(x = month, y = NPS, fill = NPS), 
+  geom_line(aes(month, Score), size = 5, alpha = .3) +
+  geom_point(aes(x = month, y = Score, fill = Score), 
              shape = 21, size = 20, color = "black", stroke = 1) +
   scale_fill_continuous(low = "darkorange", high = "yellow") +
   scale_x_continuous(breaks = seq(1,12,1)) +
-  geom_point(aes(x = (month -.35), y = (NPS + .5)), shape = 21, size = 3) +
-  geom_point(aes(x = (month + .35), y = (NPS + .5)), shape = 21, size = 3) +
+  geom_point(aes(x = (month -.35), y = (Score + .5)), shape = 21, size = 3) +
+  geom_point(aes(x = (month + .35), y = (Score + .5)), shape = 21, size = 3) +
   geom_line(aes(x = (month + x2), 
-                y = ifelse(NPS > 0, NPS + y2, NPS - (1-y2)), 
+                y = ifelse(Score > 0, Score + y2, Score - (1-y2)), 
                 group = month), size = 1) +
   ylim(-20, 20) +
   theme(panel.grid.minor = element_blank())
