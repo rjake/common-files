0712350506501d5a01e934570312dc04ae7218d0
rjake <rjake@sas.upenn.edu>
2019-04-08 01:44:27 -0400

    Create flannel_chart.R

---
+++ flannel_chart.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+
+movie_order <-
+  c(
+    "Phantom Menace", "Attack of the Clones", "Revenge of the Sith",
+    "New Hope", "Empire Strikes Back", "Return of the Jedi",
+    "Force Awakens"
+  )
+
+raw_data <-
+  starwars %>% 
+  select(films, name, species) %>% 
+  mutate(species = fct_lump(species, 2)) %>% 
+  unnest(films) %>%
+  filter(complete.cases(.)) %>% 
+  mutate(
+    films = str_remove(films, "^(The|A) "),
+    films = fct_relevel(films, movie_order)
+  ) %>% 
+  select(
+    x = films,
+    y = name,
+    color = species
+  )
+
+raw_data <- 
+  mpg %>% 
+  distinct(manufacturer, class) %>% 
+  select(
+    y = manufacturer, 
+    x = class
+  ) %>% 
+  mutate(color = 1)
+
+raw_data <- 
+  msleep %>% 
+  select(
+    x = vore, 
+    y = order
+  ) %>% 
+  distinct() %>% 
+  filter(complete.cases(.)) %>% 
+  mutate(color = 1)
+
+df <-
+  raw_data  %>% 
+  add_count(x, name = "n_y") %>% 
+  add_count(y, name = "n_x") %>% 
+  filter(n_x > 1) %>% 
+  arrange(n_x) %>%
+  #mutate(x = fct_reorder(x, n_y, max)) %>% 
+  mutate(x_int = str_pad(as.integer(factor(x)), 2, "left", "0")) %>% 
+  group_by(y) %>%
+  mutate(
+    cat = paste(unique(x_int) %>% sort(decreasing = TRUE), collapse = ",")
+  ) %>%
+  ungroup() %>%
+  mutate(y = fct_reorder(y, cat, max))
+
+ggplot(df, aes(x, y, color = color)) +
+  geom_line(aes(group = x), size = 3, color = "white") +
+  geom_line(aes(group = y), size = 3, alpha = 0.2) +
+  geom_point(size = 4) +
+  labs(color = "") +
+  coord_fixed(ratio = 0.5) +
+  theme(
+    #legend.position = "none",
+    axis.title = element_blank(),
+    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0),
+    axis.text = element_text(size = 12)
+  )
