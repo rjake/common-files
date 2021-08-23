23c03a5e0b78e747ae989790ef995870025111a3
Jake <rjake@sas.upenn.edu>
2021-08-22 22:19:56 -0400

    Create pkgdown-devtools-reprex.R

---
+++ pkgdown-devtools-reprex.R
@@ -0,0 +1,58 @@
+# creating a reprex to troubleshoot devtools::check() or pkgdown
+
+# create dummy package
+tmp <- tempdir() 
+setwd(tmp)
+
+devtools::create(path = "test")
+setwd("test")
+usethis::use_mit_license()
+
+# add R code
+writeLines(
+  con = "R/test.R",
+  text = 
+"#' does some stuff
+#' @param list for list methods
+#' @param x arg1
+#' @param y arg2
+#' @param ... some argument
+#' @export
+foo <- function(...) UseMethod('foo')
+
+#' @export
+#' @inheritParams foo
+#' @method foo default
+#' @S3method foo default 
+foo.default <- function(x, y) paste(x, y)
+
+
+#' does stuff for lists
+#' @param list for list methods
+#' @inheritParams foo
+#' @export
+#' @method foo list
+foo.list <- function(list, x, y) foo(list[[x]], list[[y]])"
+)
+
+devtools::document()
+devtools::install()
+library(test)
+
+devtools::load_all()
+
+# examples
+foo(1, 2)
+list(a = 1, b = 2) |> foo("a", "b")
+
+devtools::check(document = FALSE)
+
+# W  checking S3 generic/method consistency (561ms)
+# foo:
+#   function(list, x, y, ...)
+#     foo.default:
+#   function(x, y, ...)
+#     
+#     See section 'Generic functions and methods' in the 'Writing R
+#    Extensions' manual.
+
