bca23e63e432c57f2d129323689e72bcc2c6a972
Jake <rjake@sas.upenn.edu>
2022-10-18 14:06:38 -0400

    Update label-geom-smooth.R

--- label-geom-smooth.R
+++ label-geom-smooth.R
@@ -2,26 +2,40 @@ library(tidyverse)
 library(broom)
 
 mpg |>
-  select(
-    x = displ,
-    y = hwy,
-    group = class
-  ) |>
-  ggplot(aes(x, y, colour = group, label = group)) +
+  ggplot(aes(displ, hwy, colour = class, label = class)) +
   geom_count(alpha = 0.1) +
   stat_smooth(alpha = 0.6, method = lm, geom = "line", se = FALSE) +
   geom_text(
-    aes(y = .fitted),
-    size = 3,
-    hjust = 0,
-    nudge_x = 0.1,
+    aes(y = .fitted), size = 3, hjust = 0, nudge_x = 0.1,
     data = ~{
-      nest_by(.x, group) %>%
-      mutate(mod = list(lm(y ~ x, data = data))) |>
-      summarize(broom::augment(mod)) |>
-      arrange(x) |> 
-      slice_tail(n = 1)
+      nest_by(.x, class) |>
+        summarize(broom::augment(lm(hwy ~ displ, data = data))) |>
+        arrange(displ) |> 
+        slice_tail(n = 1)
     }
+  ) +
+  scale_x_continuous(expand = expansion(add = c(0, 1))) +
+  theme_minimal()
+
+
+# OR make a function
+last_lm_points <- function(df, formula, group) {
+  df |> 
+    nest_by({{group}}) |> 
+    summarize(broom::augment(lm(formula, data = data))) |>
+    arrange(1) |> 
+    slice_tail(n = 1)
+}
+
+last_lm_points(mpg, hwy~displ, class)
+
+mpg |>
+  ggplot(aes(displ, hwy, colour = class, label = class)) +
+  geom_count(alpha = 0.1) +
+  stat_smooth(alpha = 0.6, method = lm, geom = "line", se = FALSE) +
+  geom_text(
+    aes(y = .fitted), size = 3, hjust = 0, nudge_x = 0.1,
+    data = ~last_lm_points(.x, hwy~displ, class)
   ) +
   scale_x_continuous(expand = expansion(add = c(0, 1))) +
   theme_minimal()
