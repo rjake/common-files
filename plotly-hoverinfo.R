06546ce1ec1159370cf975ef2bc38d276583dcd2
Jake <rjake@sas.upenn.edu>
2022-11-10 14:03:05 -0500

    Update plotly-hoverinfo.R

--- plotly-hoverinfo.R
+++ plotly-hoverinfo.R
@@ -1,20 +1,34 @@
 # allows hover info to use data not in plot
+library(tidyverse)
 library(plotly)
 library(glue)
 
+# scatter plot
 economics |> 
-  mutate(
-    text = glue(
-      "Population: {pop}
-      Unemployment: {uempmed}%"
-    )
-  ) |> 
   plot_ly() |> 
   add_trace(
+    type = "scatter",
+    mode = "markers",
     x = ~date,
     y = ~uempmed,
-    text = ~text,
-    hoverinfo = ~text,
-    type = "scatter",
-    hovertemplate = "%{text}"
+    hoverinfo = "text",                  # <-----
+    hovertext =                          # <-----
+      ~glue(
+        "Population: {pop}
+        Unemployment: {uempmed}%"
+      )
   )
+
+# stacked bar chart
+diamonds |> 
+  count(cut, color) |>
+  add_count(color, wt = n, name = "total") |> 
+  plot_ly(
+    type = "bar",
+    x = ~color,
+    y = ~n,
+    name = ~cut,
+    hoverinfo = "text",                    # <-----
+    hovertext = ~paste(cut, n, "/", total) # <-----
+  ) |> 
+  layout(barmode = "stack")
